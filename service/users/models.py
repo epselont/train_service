ef999c9c180aa1df375e4341cf03198388378d0e
epselont <newrol35@gmail.com>
2022-11-25 14:36:50 +0300

    Create custom User model

---
+++ service/users/models.py
@@ -0,0 +1,89 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import (MaxLengthValidator, MinLengthValidator,
+                                    RegexValidator)
+from django.db import models
+
+MAX_LENGTH = 150
+MIN_LENGTH = 1
+MIN_LENGTH_ERR_MSG = f'Введите не менее {MIN_LENGTH} символа(-ов).'
+MAX_LENGTH_ERR_MSG = f'Введите не более {MAX_LENGTH} символа(-ов).'
+
+
+class User(AbstractUser):
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('username', 'first_name', 'last_name', 'password')
+
+    ROLES = (
+        ('ADMIN', 'Администратор'),
+        ('USER', 'Пользователь')
+    )
+
+    role = models.CharField(
+        verbose_name="Роль",
+        max_length=10,
+        choices=ROLES,
+        default='USER'
+    )
+
+    email = models.EmailField(
+        verbose_name='Почтовый адрес',
+        unique=True
+    )
+
+    username = models.CharField(
+        verbose_name='Логин',
+        max_length=MAX_LENGTH,
+        unique=True,
+        validators=[
+            MinLengthValidator(
+                MIN_LENGTH,
+                MIN_LENGTH_ERR_MSG
+            ),
+            RegexValidator(
+                '^[\w.@+-]+\Z',
+                'Можно использовать русские или латинские символы, '
+                'числа, знаки: "." и "@"'
+            ),
+            MaxLengthValidator(
+                MAX_LENGTH,
+                MAX_LENGTH_ERR_MSG
+            )
+        ]
+    )
+
+    first_name = models.CharField(
+        verbose_name='Имя пользователя',
+        max_length=MAX_LENGTH,
+        validators=[
+            MinLengthValidator(
+                MIN_LENGTH,
+                MIN_LENGTH_ERR_MSG
+            )
+        ]
+    )
+
+    last_name = models.CharField(
+        verbose_name='Фамилия Пользователя',
+        max_length=MAX_LENGTH,
+        validators=[
+            MIN_LENGTH,
+            MIN_LENGTH_ERR_MSG
+        ]
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('username',)
+
+    @property
+    def is_admin(self):
+        return self.role == 'ADMIN'
+
+    @property
+    def is_user(self):
+        return self.role == 'USER'
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
